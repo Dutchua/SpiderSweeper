288f9697ca173ecb86f54a82330ffbf0aa45a34e
Joseph Kimathi <joseph.kimathi@bbd.co.za>
2024-05-01 12:00:32 +0200

    v1 scripts

--- db_scripts/V001__create_scripts.sql
+++ db_scripts/V001__create_scripts.sql
@@ -1,11 +1,14 @@
+CREATE DATABASE SpiderSweeper;
+go
+
 CREATE TABLE [User] (
-  [UserID] int,
+  [UserID] int IDENTITY(1,1),
   [Username] varchar(50),
   PRIMARY KEY ([UserID])
 );
 
 CREATE TABLE [HighScore] (
-  [HighScoreID] int,
+  [HighScoreID] int IDENTITY(1,1),
   [UserID] int,
   [Score] int,
   [Date] datetime,
