3115626fd7aa94ffe319d10f42b20eac29ce2907
Lucinda Jordaan <Lucinda.Botes@bbd.co.za>
2024-05-05 17:00:02 +0200

    feat: build login page and navbar --wip--

--- client/script.js
+++ client/script.js
@@ -1,9 +1,12 @@
-import Navbar from "./src/components/navbar.js";
+// import Navbar from "./src/components/navbar.js";
+import LogIn from "./src/screens/logIn.js";
+import GamePage from "./src/screens/game-page.js";
 
-const click = document.getElementById("btn");
 const root = document.getElementById("root");
+root.innerHTML = GamePage();
 
-click.addEventListener("click", () => {
-  console.log("Button clicked");
-  root.innerHTML = Navbar();
-});
+// const loginButtonTag = document.getElementById("login");
+
+// loginButtonTag.addEventListener("click", () => {
+//   root.innerHTML = GamePage();
+// });
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,14 +1,21 @@
+import { LogoutButton, HighScoreButton } from "./button.js";
+
 const Navbar = () => {
-  return `<nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>`;
+  const logout = LogoutButton();
+  return (
+    `<nav class="row">
+  <section id="UserInfo" class="row">
+    <img id="Icon" src="./public/assets/profile-default.png" alt="User" />
+    <h2 id="Username">Username</h2>
+  </section>
+  <section class="row">
+    ` +
+    HighScoreButton() +
+    logout +
+    `
+  </section>
+</nav>`
+  );
 };
 
 export default Navbar;
