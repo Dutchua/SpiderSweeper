d0ed61fbec04f98bb71470d11cc51ede90879379
Lucinda Jordaan <Lucinda.Botes@bbd.co.za>
2024-05-06 21:35:39 +0200

    Add high score on game screen

--- client/src/screens/logIn.js
+++ client/src/screens/logIn.js
@@ -5,12 +5,13 @@ const LogIn = () => {
   return (
     `<section class="column">
       <img id="spider" src="./public/assets/hanging-logo.png" alt="spider" />
-      <h1 class="creepster-regular">Welcome To Spider Sweeper</h1>
+      <h1 >Welcome To Spider Sweeper</h1>
       ` +
     loginButton +
     `
     </section>`
   );
 };
+// class="creepster-regular"
 
 export default LogIn;
