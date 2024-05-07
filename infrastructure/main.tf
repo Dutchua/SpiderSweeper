b116bdd1fa02770290e0933ba04c414b5055bb40
Josh Jennings <josh@bbd.co.za>
2024-05-07 14:03:10 +0200

    I hate subnets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -57,7 +57,7 @@ resource "aws_route_table" "r" {
 }
 
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.spidersweeper_subnet_a.id
+  subnet_id      = aws_subnet.spidersweeper_subnet_b.id
   route_table_id = aws_route_table.r.id
 }
 
