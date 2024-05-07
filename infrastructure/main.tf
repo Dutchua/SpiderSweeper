2134fe9fc8c143ee4a7b10bd6f1f6ab67029c086
Josh Jennings <josh@bbd.co.za>
2024-05-07 11:04:57 +0200

    wowza

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,7 +18,7 @@ variable "aws_region" {
 provider "aws" {
 }
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "default_main" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
@@ -28,27 +28,27 @@ resource "aws_vpc" "main" {
 }
 
 
-resource "aws_subnet" "main_a" {
-  vpc_id                  = aws_vpc.main.id
+resource "aws_subnet" "default_main_a" {
+  vpc_id                  = aws_vpc.default_main.id
   cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-west-1a"
 }
 
-resource "aws_subnet" "main_b" {
-  vpc_id                  = aws_vpc.main.id
+resource "aws_subnet" "default_main_b" {
+  vpc_id                  = aws_vpc.default_main.id
   cidr_block              = "10.0.5.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-west-1b"
 }
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.default_main.id
 }
 
 
 resource "aws_route_table" "r" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.default_main.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -57,7 +57,7 @@ resource "aws_route_table" "r" {
 }
 
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main_a.id
+  subnet_id      = aws_subnet.default_main_a.id
   route_table_id = aws_route_table.r.id
 }
 
@@ -75,13 +75,13 @@ resource "aws_db_instance" "mssql" {
   vpc_security_group_ids = [aws_security_group.mssql_sg.id]
 }
 
-resource "aws_db_subnet_group" "main" {
-  name       = "main"
-  subnet_ids = [aws_subnet.main_a.id, aws_subnet.main_b.id]
+resource "aws_db_subnet_group" "default_main_subnet" {
+  name       = "default_main_subnet"
+  subnet_ids = [aws_subnet.default_main_a.id, aws_subnet.default_main_b.id]
 }
 
 resource "aws_security_group" "mssql_sg" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.default_main_subnet.id
 
   ingress {
     from_port   = 1433
