b1ef706da74292d1d90cb0b29dad61c16bf61c17
Josh Jennings <josh@bbd.co.za>
2024-05-07 04:40:35 +0200

    asassadwdqw

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,7 +19,7 @@ provider "aws" {
 }
 
 resource "aws_default_vpc" "default_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/0"
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
