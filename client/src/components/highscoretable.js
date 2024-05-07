3b70ec344a42cf8a41cf941ad02c6a8f3827bbca
christine-olckers-bbd <christine.olckers@bbd.co.za>
2024-05-07 18:01:31 +0200

    HighScoreTable added to Java

---
+++ client/src/components/highscoretable.js
@@ -0,0 +1,30 @@
+import { HighScoreButton, HighScoreButtonMini} from "./button.js";
+
+const HighScoreTable = () => {
+    return (
+      `<section  id="GameFunctions" class="space-between background">
+      <section class="row">` +
+      StopWatch() +
+      FlagCount({ count: 10 }) +
+      `</section>` +
+      NewGameButton() +
+      `
+  </section>`
+  `<section>` + 
+    `<table id="HighScoreTable">
+        <tr>
+            <td class="HighScoreText">High Score 1:</td>
+            <td>01:15</td>
+            <td>2024/05/07</td>
+        </tr>
+        <tr>
+            <td class="HighScoreText">High Score 2:</td>
+            <td>--</td>
+            <td>--</td>
+        </tr>
+        </table>` +
+    `</section>`
+    );
+  };
+
+  export default HighScoreTable;
