7735e4229051777278331d14a59a6c659a2cb258
Josh Jennings <josh@bbd.co.za>
2024-05-09 00:05:24 +0200

    asdasd

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -56,7 +56,7 @@ resource "aws_s3_bucket_policy" "existing_bucket_policy" {
 
 # Upload files to S3 bucket
 resource "aws_s3_object" "existing_bucket_files" {
-  for_each     = fileset("../frontend/Views/", "*")
+  for_each     = fileset("../frontend/Views/", "**/*")
   bucket       = data.aws_s3_bucket.existing_bucket.id
   key          = each.value
   source       = "../frontend/Views/${each.value}"
