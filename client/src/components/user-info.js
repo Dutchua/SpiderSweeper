ecf2ef591d3024186b73dac0789421547b970100
Lucinda Jordaan <Lucinda.Botes@bbd.co.za>
2024-05-09 16:58:57 +0200

    Add username to ui

--- client/src/components/user-info.js
+++ client/src/components/user-info.js
@@ -1,7 +1,9 @@
 const UserInfo = () => {
+  const user = sessionStorage.getItem("username");
+
   return `<section id="UserInfo" class="row">
   <img id="Icon" src="./public/assets/profile-default.png" alt="User" />
-  <h2 id="Username">Username</h2>
+  <h2 id="Username">${user}</h2>
 </section>`;
 };
 
